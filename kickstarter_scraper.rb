d24a888f65117129811f433a70dcf7c9c25b6c4d
Winston  Chung <18.chung.winston@ctkprep.org>
2019-09-14 00:15:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,10 +8,14 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => 
+      
+    }
   end
   projects
-  binding.pry
+  binding
 end
 
 create_project_hash
